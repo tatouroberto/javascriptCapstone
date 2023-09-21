de6ea1463714ff47177379a29d0d966b71507a05
Ivonne <ivonnebenites2015@gmail.com>
2023-09-21 16:22:20 -0500

    Chores: update file

--- src/modules/gettingMovies.js
+++ src/modules/gettingMovies.js
@@ -1,7 +1,7 @@
 const gettingMovie = async () => {
   const response = await fetch('https://api.tvmaze.com/shows');
   const getMovie = await response.json();
-  const listOfMovies = getMovie.slice(7, 20);
+  const listOfMovies = getMovie.slice(14, 20);
   return listOfMovies;
 };
 
