29b9447f75eaf0758735a00aa36001110a97c23d
tatouroberto <tatouroberto@gmail.com>
2023-09-23 23:47:05 +0200

    clear main

--- src/modules/countMovies.js
+++
@@ -1,7 +0,0 @@
-const movieCounter = () => {
-  const movieCounter = document.querySelector('.main-section');
-  const count = movieCounter.childElementCount;
-  return count;
-};
-
-export default movieCounter;
--- src/modules/gettingMovies.js
+++
@@ -1,8 +0,0 @@
-const gettingMovie = async () => {
-  const response = await fetch('https://api.tvmaze.com/shows');
-  const getMovie = await response.json();
-  const listOfMovies = getMovie.slice(35, 45);
-  return listOfMovies;
-};
-
-export default gettingMovie;
